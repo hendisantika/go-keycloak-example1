ea3d8a0f5045395bdcbaf0e390a6cdd0bdd4de60
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 14:34:43 +0700

    feat: Add newMiddleware🫸🌀✏️📗🐧🐳

--- middleware.go
+++ middleware.go
@@ -3,3 +3,7 @@ package main
 type keyCloakMiddleware struct {
 	keycloak *keycloak
 }
+
+func newMiddleware(keycloak *keycloak) *keyCloakMiddleware {
+	return &keyCloakMiddleware{keycloak: keycloak}
+}

c1454fda1590dcc1221d5a4f1f67963b3352f779
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 14:34:53 +0700

    feat: Add extractBearerToken🫸🌀✏️📗🐧🐳

--- middleware.go
+++ middleware.go
@@ -1,9 +1,15 @@
 package main
 
+import "strings"
+
 type keyCloakMiddleware struct {
 	keycloak *keycloak
 }
 
 func newMiddleware(keycloak *keycloak) *keyCloakMiddleware {
 	return &keyCloakMiddleware{keycloak: keycloak}
+}
+
+func (auth *keyCloakMiddleware) extractBearerToken(token string) string {
+	return strings.Replace(token, "Bearer ", "", 1)
 }

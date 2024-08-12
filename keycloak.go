096f9cea12557257d1777604de3e191a5a275ec9
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 14:32:54 +0700

    feat: Add keycloak model🫸🌀✏️📗🐧🐳

---
+++ keycloak.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/Nerzal/gocloak/v13"
+
+type keycloak struct {
+	gocloak      gocloak.GoCloak // keycloak client
+	clientId     string          // clientId specified in Keycloak
+	clientSecret string          // client secret specified in Keycloak
+	realm        string          // realm specified in Keycloak
+}

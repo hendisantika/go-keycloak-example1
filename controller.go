154203e006c05f053f99ec978e93524006b66013
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 14:41:17 +0700

    feat: Add loginResponse🫸🌀✏️📗🐧🐳

--- controller.go
+++ controller.go
@@ -12,3 +12,9 @@ type loginRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+type loginResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+	ExpiresIn    int    `json:"expiresIn"`
+}

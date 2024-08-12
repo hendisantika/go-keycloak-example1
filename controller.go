dd59180787e64812c3537f2460861dd890d004ee
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 14:42:09 +0700

    feat: Add login🫸🌀✏️📗🐧🐳

--- controller.go
+++ controller.go
@@ -1,6 +1,11 @@
 package main
 
-import "time"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
 type doc struct {
 	Id   string    `json:"id"`
@@ -27,4 +32,37 @@ func newController(keycloak *keycloak) *controller {
 	return &controller{
 		keycloak: keycloak,
 	}
+}
+
+func (c *controller) login(w http.ResponseWriter, r *http.Request) {
+	rq := &loginRequest{}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(rq); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	jwt, err := c.keycloak.gocloak.Login(context.Background(),
+		c.keycloak.clientId,
+		c.keycloak.clientSecret,
+		c.keycloak.realm,
+		rq.Username,
+		rq.Password)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+
+	rs := &loginResponse{
+		AccessToken:  jwt.AccessToken,
+		RefreshToken: jwt.RefreshToken,
+		ExpiresIn:    jwt.ExpiresIn,
+	}
+
+	rsJs, _ := json.Marshal(rs)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(rsJs)
 }

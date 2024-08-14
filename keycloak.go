607ec3ee4ff9125959e021f317d8c6ed80ce354a
hendisantika <hendisantika@yahoo.co.id>
2024-08-14 15:45:03 +0700

    fix: codes :three:🫸🌀✏️📗🐧🐳

--- keycloak.go
+++ keycloak.go
@@ -3,7 +3,6 @@ package main
 import "github.com/Nerzal/gocloak/v13"
 
 type keycloak struct {
-	//gocloak      gocloak.GoCloak // keycloak client
 	client       *gocloak.GoCloak // keycloak client
 	realm        string           // realm specified in Keycloak
 	clientId     string           // clientId specified in Keycloak
@@ -12,8 +11,7 @@ type keycloak struct {
 
 func newKeycloak() *keycloak {
 	return &keycloak{
-		client: gocloak.NewClient("http://localhost:8080"),
-		//client := gocloak.NewClient(URL, gocloak.SetAuthAdminRealms("admin/realms"), gocloak.SetAuthRealms("realms")),
+		client:       gocloak.NewClient("http://localhost:8080"),
 		realm:        "PowerRanger",
 		clientId:     "my-go-service",
 		clientSecret: "abfa2984-9125-486b-b360-03386ad13e08",
